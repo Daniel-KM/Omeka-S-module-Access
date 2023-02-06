720d9e73c23533be8bda5c63cc3f1f99d0b20765
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-02-06 00:00:00 +0000

    Used Omeka S v4 feature "element group" for settings.

--- src/Form/SettingsFieldset.php
+++ src/Form/SettingsFieldset.php
@@ -10,14 +10,20 @@ class SettingsFieldset extends Fieldset
 {
     protected $label = 'Access resource'; // @translate
 
+    protected $elementGroups = [
+        'access' => 'Access resource', // @translate
+    ];
+
     public function init(): void
     {
         $this
             ->setAttribute('id', 'access-resource')
+            ->setOption('element_groups', $this->elementGroups)
             ->add([
                 'name' => 'accessresource_message_send',
                 'type' => Element\Checkbox::class,
                 'options' => [
+                    'element_group' => 'access',
                     'label' => 'Send email to admin and user on access request or update', // @translate
                 ],
                 'attributes' => [
@@ -29,6 +35,7 @@ class SettingsFieldset extends Fieldset
                 'name' => 'accessresource_message_admin_subject',
                 'type' => Element\Text::class,
                 'options' => [
+                    'element_group' => 'access',
                     'label' => 'Admin email subject', // @translate
                 ],
                 'attributes' => [
@@ -39,6 +46,7 @@ class SettingsFieldset extends Fieldset
                 'name' => 'accessresource_message_admin_request_created',
                 'type' => CkeditorInline::class,
                 'options' => [
+                    'element_group' => 'access',
                     'label' => 'Message to admin for new request', // @translate
                 ],
                 'attributes' => [
@@ -49,6 +57,7 @@ class SettingsFieldset extends Fieldset
                 'name' => 'accessresource_message_admin_request_updated',
                 'type' => CkeditorInline::class,
                 'options' => [
+                    'element_group' => 'access',
                     'label' => 'Message to admin for updated request', // @translate
                 ],
                 'attributes' => [
@@ -60,6 +69,7 @@ class SettingsFieldset extends Fieldset
                 'name' => 'accessresource_message_user_subject',
                 'type' => Element\Text::class,
                 'options' => [
+                    'element_group' => 'access',
                     'label' => 'User email subject', // @translate
                 ],
                 'attributes' => [
@@ -70,6 +80,7 @@ class SettingsFieldset extends Fieldset
                 'name' => 'accessresource_message_user_request_created',
                 'type' => CkeditorInline::class,
                 'options' => [
+                    'element_group' => 'access',
                     'label' => 'Message to user for new request', // @translate
                 ],
                 'attributes' => [
@@ -80,6 +91,7 @@ class SettingsFieldset extends Fieldset
                 'name' => 'accessresource_message_user_request_updated',
                 'type' => CkeditorInline::class,
                 'options' => [
+                    'element_group' => 'access',
                     'label' => 'Message to user for updated request', // @translate
                 ],
                 'attributes' => [
