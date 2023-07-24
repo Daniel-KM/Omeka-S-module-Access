50a018f7046fc35c08cda4666c66e174fba8bee6
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-07-24 00:00:00 +0000

    Updated admin logs.

--- src/Controller/Admin/LogController.php
+++ src/Controller/Admin/LogController.php
@@ -2,6 +2,7 @@
 
 namespace AccessResource\Controller\Admin;
 
+use AccessResource\Entity\AccessLog;
 use Doctrine\ORM\EntityManager;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
@@ -27,6 +28,7 @@ class LogController extends AbstractActionController
 
     public function browseAction()
     {
+        /*
         $this->browse()->setDefaults('access_logs');
         $response = $this->api()->search('access_logs', $this->params()->fromQuery());
         $this->paginator($response->getTotalResults());
@@ -35,6 +37,37 @@ class LogController extends AbstractActionController
         unset($returnQuery['page']);
 
         $accessLogs = $response->getContent();
+        */
+
+        $params = $this->params();
+        $page = $params->fromQuery('page', 1);
+        $perPage = $this->settings()->get('pagination_per_page', 25);
+
+        $query = $params->fromQuery() + [
+            'page' => $page,
+            'per_page' => $perPage,
+            'sort_by' => $params->fromQuery('sort_by', 'id'),
+            'sort_order' => $params->fromQuery('sort_order', 'desc'),
+        ];
+
+        $qb = $this->entityManager->createQueryBuilder();
+        $qb
+            ->select('logs')
+            ->from(AccessLog::class, 'logs')
+            ->setFirstResult(((int) $query['page'] - 1) * (int) $query['per_page'])
+            ->setMaxResults((int) $query['per_page'])
+            ->orderBy('logs.id', 'DESC');
+        $accessLogs = $qb->getQuery()->getResult();
+
+        $qb = $this->entityManager->createQueryBuilder();
+
+        $logCount = $qb
+            ->select($qb->expr()->count('logs_count.id'))
+            ->from(AccessLog::class, 'logs_count')
+            ->getQuery()
+            ->getSingleResult();
+
+        $this->paginator($logCount, $page, $perPage);
 
         return new ViewModel([
             'accessLogs' => $accessLogs,
